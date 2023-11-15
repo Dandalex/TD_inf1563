e413147ecdee938cd1c4fa51ed91ebf09a17b46c
Dandalex <dana16@uqo.ca>
2023-11-15 15:16:10 -0500

    début de la classe

---
+++ inf1563_TD2/src/inf1563_TD2/loginEtMotDePasse.java
@@ -0,0 +1,19 @@
+package inf1563_TD2;
+import java.util.Scanner;
+public class loginEtMotDePasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static string genererLogin(string nom,string prenom) {
+		if (n<10) {
+			return n;
+		}
+		else {
+		int somme= sommeTotale (sommeDesChiffres(n));
+		return somme;
+		}
+	}
+}
